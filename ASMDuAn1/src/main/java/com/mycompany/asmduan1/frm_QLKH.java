1c60abb24ac97b23448a8fb3e2d64f7cdc443b8b
ncdao22103 <nguyenchidao117@gmail.com>
2024-07-29 14:25:05 +0700

    quat

--- ASMDuAn1/src/main/java/com/mycompany/asmduan1/frm_QLKH.java
+++ ASMDuAn1/src/main/java/com/mycompany/asmduan1/frm_QLKH.java
@@ -327,6 +327,7 @@ public class frm_QLKH extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void btn_SuaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_SuaActionPerformed
